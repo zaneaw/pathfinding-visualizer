3b19a1de433e2333ae32315b593ed9a9c94f7642
Zane Wilson <wilsonzanea@gmail.com>
2023-01-10 16:19:35 -0500

    algoIsRunning to disable buttons

--- components/ToolbarButton.tsx
+++ components/ToolbarButton.tsx
@@ -5,6 +5,7 @@ type Props = {
     title: string;
     icon: any;
     styles: any;
+    isAlgoRunning: boolean | null;
 };
 
 const ToolbarButton: React.FC<Props> = ({
@@ -12,15 +13,17 @@ const ToolbarButton: React.FC<Props> = ({
     title,
     icon,
     styles,
+    isAlgoRunning,
 }) => {
     return (
-        <div
+        <button
             onClick={handleClick}
-            className={`flex flex-row justify-center items-center gap-1 py-1 px-2 text-center rounded border-2 border-slate-500 hover:bg-slate-500 hover:cursor-pointer ${styles}`}
+            className={`flex flex-row justify-center items-center gap-1 py-1 px-2 text-center rounded border-2 border-slate-500 hover:cursor-pointer transition-all ease-in duration-150 ${styles} ${isAlgoRunning ? 'bg-slate-500' : 'hover:bg-green-500'}`}
+            disabled={isAlgoRunning ? true : false}
         >
             {title}
             {icon ? icon : ''}
-        </div>
+        </button>
     );
 };
 
