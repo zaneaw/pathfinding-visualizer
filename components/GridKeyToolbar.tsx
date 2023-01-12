522bee8259694301cc37422b5b0f554334535f31
Zane Wilson <wilsonzanea@gmail.com>
2023-01-11 22:36:17 -0500

    rearrange + weighted node key

--- components/GridKeyToolbar.tsx
+++ components/GridKeyToolbar.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import GridKeyOption from './GridKeyOption';
 import { FiMapPin } from 'react-icons/fi';
-import { GiFinishLine } from 'react-icons/gi';
+import { GiFinishLine, GiStoneWall, GiWeight } from 'react-icons/gi';
 
 const GridKeyToolbar = () => {
     return (
         <div className='flex flex-row items-center justify-around flex-wrap px-2 pb-2 gap-2'>
             <GridKeyOption keyName="Node" />
-            <GridKeyOption keyName="Start" KeyIcon={FiMapPin} keyStyles="bg-green-500" />
-            <GridKeyOption keyName="End" KeyIcon={GiFinishLine} keyStyles="bg-red-500" />
-            <GridKeyOption keyName="Wall" keyStyles="bg-slate-500" />
+            <GridKeyOption keyName="Start" keyStyles="bg-green-500" KeyIcon={FiMapPin} />
+            <GridKeyOption keyName="End" keyStyles="bg-red-500" KeyIcon={GiFinishLine} />
+            <GridKeyOption keyName="Wall" keyStyles="bg-slate-500" KeyIcon={GiStoneWall} />
+            <GridKeyOption keyName="Weighted" keyStyles="bg-blue-500" KeyIcon={GiWeight} />
             <GridKeyOption keyName="Visited" keyStyles="bg-purple-500" />
             <GridKeyOption keyName="Shortest Path" keyStyles="bg-yellow-500" />
         </div>
